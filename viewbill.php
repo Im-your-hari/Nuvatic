f8f457171c27940f9483af658d9f6b5c363d6d72
Im-your-hari <baluoger91@gmail.com>
2023-01-28 22:57:48 +0530

    Bill view added for pdf invoice,Lpo,Quotation etc..Need to add mpdf and pdfs

--- viewbill.php
+++ viewbill.php
@@ -1,28 +1,22 @@
-<!--Admin page-->
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Bills : Nuvatic</title>
+	<title>View Bill : Nuvatic</title>
+</head>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
-</head>
 <body>
-
-
-	<div class="container">
+<div class="container">
 		<hr>
-		<h1 class="text-center text-info">Nuvatic : Admin</h1>
+		<h1 class="text-center text-info">Nuvatic : View Bills</h1>
 		<hr>
-		<a href="index.php">
-			<button class="btn btn-info ml-auto">HOME</button>
+		<a href="admin.php">
+			<button class="btn btn-info ml-auto">BACK</button>
 		</a>
 		
 		<hr>
@@ -34,44 +28,40 @@
 				<th>#</th>
 				<th>#</th>
 			</tr>
-			
-				<?php
-					include("config.php");
-
-					$sql = "show tables";
-					$result = mysqli_query($conn,$sql);
-					if ($result) {
-						while($row=mysqli_fetch_assoc($result)){
-							if($row['Tables_in_nuvatic']=='products' || $row['Tables_in_nuvatic']=='users'){
-								continue;
-							}else{
-								echo "<tr>";
-								echo "<td>".$row['Tables_in_nuvatic']."<br></td>";
-								
-								echo "<td>
-								<a href='viewbill.php?name=".$row['Tables_in_nuvatic']."'>
-								<input type='button' class='btn btn-info text-white' name='view' id='view' value='VIEW'></a></td>";
-
-								echo "<td>
-								<a href='updatetable.php?name=".$row['Tables_in_nuvatic']."'>
-								<input type='button' class='btn btn-warning text-white' name='update' id='update' value='UPDATE'></a></td>";
-
-								echo "<td>
-								<a href='deletetable.php?name=".$row['Tables_in_nuvatic']."'>
-								<input type='button' class='btn btn-danger text-white' name='delete' id='delete' value='DELETE'></a></td>";
-								echo "</tr>";
-							}
-							
-						}
-					}
-				?>
 
 
-</table>
+<?php
+	include("config.php");
+
+	$tablename = $_GET['name'];
+
+	$sql = "select * from ".$tablename;
+	$result = mysqli_query($conn,$sql);
+	if ($result) {
 		
-	</div>
+		while($row=mysqli_fetch_assoc($result)){
 
-</center>
+			echo "<tr>";
+			echo "<td>".$row['product']."<br></td>";
+			echo "<td>".$row['price']."<br></td>";
+			echo "<td>".$row['unit']."<br></td>";
+			echo "<td>".$row['date']."<br></td>";
+			
+			echo "</tr>";
+			
+		}
+
+		echo "<div class='container p-3'><br><hr><a href='invoice.php'><button class='btn btn-outline-danger'>Generate Invoice</button></a>&nbsp;";
+		echo "<a href='lpo.php'><button class='btn btn-outline-warning'>Generate LPO</button></a>&nbsp;";
+		echo "<a href='quote.php'><button class='btn btn-outline-success'>Generate Quotation</button></a>&nbsp;</div>";
+
+
+	}else{
+		echo "something went wrong";
+	}
+?>
+
+
 
 
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
