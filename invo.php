4eb81ee7af47ed50ef1e2961a82ea2b0591842e7
Im-your-hari <baluoger91@gmail.com>
2023-03-15 23:31:22 +0530

    LPO Almost set..Neet to add Delivery note and Change some designs

---
+++ invo.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .invo{
+        width: 100%;
+        font-family:Arial, Helvetica, sans-serif;
+        height: 1500px;
+      }
+      .center {
+        margin: auto;
+        width: 70%;
+      }
+      table,th,td{
+          border: 1px solid black;
+          border-collapse: collapse;
+          
+      }
+      td{
+        white-space:nowrap;
+      }
+      .no_br{
+          border-top: none;
+          border-bottom: none;
+      }
+      .grid-container {
+  display: grid;
+  grid-template-columns: auto auto;
+  width: 80%;
+}
+
+    </style>
+</head>
+
+<body>
+    <section class="invo center">
+        <div class="header" style="text-align: center; padding: 30px;">
+            <div class="image" style="display: inline-block; "><img style="margin-bottom: -40px;" src="logo.PNG" width="120px" height="120px"></div>
+            <div class="heading" style="display: inline-block;">
+                <h1 style="color: rgb(40, 40, 117); font-size: 40px; font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif">Nuvatic Electrical Equipment Trading</h2>
+            </div>
+        </div>
+        <div style="width: 100%; text-align: center; margin-bottom: -20px;" class="tableName">
+            <h4 >Tax Invoice</h6>
+        </div>
+        <div class="table1" style="width: 100%;">
+            <table style="width:100%">
+                <tr>
+                    <td colspan="2" rowspan="3" > 
+                        <p>
+                            <b>Nuvatic Electrical Equipment Trading</b><br>
+                            Po Box: 12536, Mussafah 44<br>
+                            Tel: +97126773688<br>
+                            Emirate: Abu Dhabi<br>
+                            TRN: 100541904700003
+                        </p>
+                    </td>
+                    <td>
+                        <p>Invoice No.<br><b>NR-2023/4709</b></p>
+                    </td>
+                    <td>
+                        <p>Dated<br><b>27-Jan-23</b></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Delivery Note<br><b>5831</b></p>
+                    </td>
+                    <td>
+                        <p>Mode/Terms of Payment<br><b>30 Days</b></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height: 60px;">
+                        <p style="margin-top: -1px;">Supplier's Ref</p>
+                    </td>
+                    <td>
+                        <p style="margin-top: -1px;">Other References(s)</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" rowspan="4">
+                        <p style="margin-top: -100px;">Buyer</p>
+                        <p><br>
+                            <b>KALPATARU POWER TRANSMISSION LIMITED</b><br>
+                            UNIT NO. 2607. FLOOR-26TH<br>
+                            AL SHAFAR TOWER-1<br>
+                            AL HAKSHA HEIGHT (TECOM)
+                            <div style="margin-top: -10px;">
+                                <div class="grid-container">
+                                    <div>Emirate</div>
+                                    <div>:  Dubai</div>
+                                    <div>Country</div>  
+                                    <div>:  UAE</div>
+                                    <div>TRN</div>
+                                    <div>:  10031362210003</div>
+                                    <div>Place of supply</div>
+                                    <div>:  UAE, Abu Dhabi</div>
+                                  </div>
+                            </div>
+                        </p>
+                    </td>
+                    <td>
+                        <p>Buyer's Order No.<br><b>2400120034</b></p>
+                    </td>
+                    <td>
+                        <p>Dated<br><b>27-Jan-23</b></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p style="margin-top: -5px;">Despatch Document No.</p>
+                    </td>
+                    <td>
+                        <p>Delivery Note Date<br><b>27-Jan-23</b></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height: 60px;">
+                        <p style="margin-top: -1px;">Despatched through</p>
+                    </td>
+                    <td>
+                        <p style="margin-top: -1px;">Destination</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="height: 120px;"><p style="margin-top: -60px;">Terms of Delivery</p></td>
+                </tr>
+            </table>
+            <table style="width: 100%;" class="no_br">
+                <tr>
+                    <th>SL<br>No</th>
+                    <th>Description of Goods</th>
+                    <th>Quality</th>
+                    <th>Rate</th>
+                    <th>per</th>
+                    <th>VAT<br>%</th>
+                    <th>Amount</th>
+                    <th>Taxable<br>Vlaue(AED)</th>
+                    <th>VAT<br>(AED)</th>
+                </tr>
+                <tr>
+                    <td class="no_br">1</td>
+                    <td class="no_br"><b>FLEXIBLE CABLE 2.5MM X 3C RR</b></td>
+                    <td class="no_br"><b>4 ROLL</b></td>
+                    <td class="no_br">185.00</td>
+                    <td class="no_br">ROLL</td>
+                    <td class="no_br">5 %</td>
+                    <td class="no_br"><b>740.00</b></td>
+                    <td class="no_br">740.00</td>
+                    <td class="no_br">37.00</td>
+                </tr>
+                <tr>
+                    <td class="no_br">2</td>
+                    <td class="no_br"><b>LED FLOOD LIGHT 100W 220V</b></td>
+                    <td class="no_br"><b>6 ROLL</b></td>
+                    <td class="no_br">35.00</td>
+                    <td class="no_br">NOS</td>
+                    <td class="no_br">5 %</td>
+                    <td class="no_br"><b>210.00</b></td>
+                    <td class="no_br">210.00</td>
+                    <td class="no_br">10.50</td>
+                </tr>
+                <tr>
+                    <td class="no_br">3</td>
+                    <td class="no_br"><b>GIFFEX GLAND TYPE</b></td>
+                    <td class="no_br"><b>1 ROLL</b></td>
+                    <td class="no_br">12.00</td>
+                    <td class="no_br">NOS</td>
+                    <td class="no_br">5 %</td>
+                    <td class="no_br"><b>12.00</b></td>
+                    <td class="no_br">12.00</td>
+                    <td class="no_br">0.60</td>
+                </tr>
+                <tr>
+                    <td class="no_br">4</td>
+                    <td class="no_br"><b>COURIER CHARGE MUSSAFAH TO AL AIN</b></td>
+                    <td class="no_br"><b>1 ROLL</b></td>
+                    <td class="no_br">50.00</td>
+                    <td class="no_br">NOS</td>
+                    <td class="no_br">5 %</td>
+                    <td class="no_br"><b>50.00</b></td>
+                    <td class="no_br">50.00</td>
+                    <td class="no_br">2.50</td>
+                </tr>
+                <tr>
+                    <!-- for spacing need only-->
+                    <td class="no_br" style="height: 60px;"></td> 
+                    <td class="no_br"></td>
+                    <td class="no_br"></td>
+                    <td class="no_br"></td>
+                    <td class="no_br"></td>
+                    <td class="no_br"></td>
+                    <td class="no_br"></td>
+                    <td class="no_br"></td>
+                    <td class="no_br"></td>
+                </tr>
+                <tr>
+                    <td colspan="9">
+                        <div style="float: left;">
+                            <p style="height: 200px;">Amount Chargeable(in words)<br>
+                                <b>UAE Dirham One Thousand Sixty Two and Sixty fils Only(AED 1,062.60)</b>
+                                <br>VAT Amount (in words)<br>
+                                <b>UAE Dirham Fifty and Sixty fils Only (AED 50.60)</b>
+                                <div style="width: 50px;"></div>
+                                <p><u>Declaration</u></p>
+                                <div style="width: 400px;">We declare that this invoice shows the actual price of the<br> goods described and that all particulars are true and correct</div>
+                            </p>
+                        </div>
+                        <div style="float: right;" style=" width: 100%;">
+                            <div class="grid-container" style="margin-top: -2px; margin-right: -2px; width: 300px; border: 1px solid black;">
+                                <div>Taxable Value</div>
+                                <div>1012.00</div>
+                                <div>Value Added Tax 5%</div>  
+                                <div>50.60</div>
+                                <div style="border-top: 1px solid; border-bottom: 1px solid;"><b>Invoice Total</b></div>
+                                <div style="border-top: 1px solid; border-bottom: 1px solid;"><b>1062.60</b></div>
+                                
+                                
+                              </div><div style="float: right;">E. & O.E</div>
+                        </div>
+                        
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5"><p style="margin-top: -47px;">Customer's Seal and Signature</p></td>
+                    <td colspan="4"><div>
+                        <div>
+                            <p style="float: right; margin-top: -1px;">for Nuvatic Electrical Equipment Trading</p>
+                        </div>
+                        <div>
+                            <p style="float: right; margin-top: 20px;">Authorised Signatory</p>
+                        </div>
+                    </div></td>
+                </tr>
+            </table>
+            <div><p style="width: 100%; text-align: center;" class="center">This is a Computer Generated invoice</p></div>
+        </div>
+    </section>
+</body>
+</html>
